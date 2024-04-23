2985413ba2eae0aa80f3f26deadaa00af5009428
bernejorge <bernejorge@gmail.com>
2024-04-23 12:36:11 -0300

    estados viajes controller

---
+++ src/controllers/estados_viajes.controller.mjs
@@ -0,0 +1,9 @@
+import db from "./../models/loader.mjs"; // Asegúrate de ajustar la ruta al archivo loader
+import { crudControllerFactory } from "./crudControllerFactory.mjs";
+
+const { EstadosViajes } = db;
+
+// Usar la fábrica para crear las funciones CRUD básicas
+const EstadosViajesController = crudControllerFactory(db.EstadosViajes);
+
+export default EstadosViajesController;
